583ef1465306e11f2b712d3398263d0d051c23cb
Tatiana <tany663@gmail.com>
2026-04-10 21:12:11 +0300

    change upload

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,6 +1,7 @@
 import { Readable } from 'node:stream';
 import { v2 as cloudinary } from 'cloudinary';
 
+
 cloudinary.config({
   secure: true,
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -8,13 +9,12 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export async function saveFileToCloudinary(buffer, userId) {
+export async function saveFileToCloudinary(buffer) {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       {
-        folder: 'notes-app/avatars',
+        folder: 'users/avatars',
         resource_type: 'image',
-        public_id: `avatar_${userId}`,
         overwrite: true,
         unique_filename: false,
       },
